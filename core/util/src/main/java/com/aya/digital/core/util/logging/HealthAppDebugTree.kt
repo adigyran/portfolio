b0b628c07cb5808d271659a85ac7033749c61401
adigyran <adigyran@gmail.com>
2023-01-25 14:33:20 +0300

    timber log with hyperlink

--- core/util/src/main/java/com/aya/digital/core/util/logging/HealthAppDebugTree.kt
+++ core/util/src/main/java/com/aya/digital/core/util/logging/HealthAppDebugTree.kt
@@ -3,7 +3,16 @@ package com.aya.digital.core.util.logging
 import timber.log.Timber
 
 class HealthAppDebugTree : Timber.DebugTree() {
-    override fun createStackElementTag(element: StackTraceElement): String? {
-        return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
+
+    private val hyperLink : String
+        get() = Thread.currentThread().stackTrace.let { elements: Array<StackTraceElement> ->
+            val index = 7 // most relevant StackTraceElement index that actually prints to logcat
+            elements[index].let {
+                "(${it.fileName}:${it.lineNumber}) "
+            }
+        }
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        super.log(priority, tag, hyperLink + message, t)
     }
 }
