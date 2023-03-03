a44d736333e793e2d21b3c97a1785b8045d9d9c1
adigyran <adigyran@gmail.com>
2023-03-03 20:47:59 +0300

    profile screens

--- feature/profile/generalinfo/edit/src/main/java/com/aya/digital/core/feature/profile/generalinfo/edit/ui/model/ProfileGeneralInfoEditStateTransformer.kt
+++ feature/profile/generalinfo/edit/src/main/java/com/aya/digital/core/feature/profile/generalinfo/edit/ui/model/ProfileGeneralInfoEditStateTransformer.kt
@@ -7,20 +7,34 @@ import com.aya.digital.core.model.ProfileSex
 import com.aya.digital.core.model.getSexName
 import com.aya.digital.core.mvi.BaseStateTransformer
 import com.aya.digital.core.ui.adapters.base.DiffItem
+import com.aya.digital.core.ui.delegates.components.fields.name.model.NameFieldUIModel
 import com.aya.digital.core.ui.delegates.components.fields.selection.model.DropdownFieldUIModel
+import com.aya.digital.core.ui.delegates.components.fields.selection.model.SelectionFieldUIModel
+import java.time.LocalDate
 
 class ProfileGeneralInfoEditStateTransformer(private val context : Context): BaseStateTransformer<ProfileGeneralInfoEditState, ProfileGeneralInfoEditUiModel>() {
     override fun invoke(state: ProfileGeneralInfoEditState): ProfileGeneralInfoEditUiModel =
         ProfileGeneralInfoEditUiModel(
             data = kotlin.run {
                return@run mutableListOf<DiffItem>().apply {
+                   add(NameFieldUIModel(FieldsTags.FIRST_NAME_FIELD_TAG,"First Name",state.firstName.getField(),null))
+                   add(NameFieldUIModel(FieldsTags.LAST_NAME_FIELD_TAG,"Last Name",state.lastName.getField(),null))
+                   add(NameFieldUIModel(FieldsTags.MIDDLE_NAME_FIELD_TAG,"Middle Name",state.middleName.getField(),null))
+                   add(SelectionFieldUIModel(FieldsTags.BIRTH_DATE_FIELD_TAG,"Date of Birth",state.birthDate.getField(),null))
                    add(DropdownFieldUIModel(FieldsTags.SEX_FIELD_TAG,getSexesList(),"Sex","",null))
-                }
+                   add(NameFieldUIModel(FieldsTags.HEIGHT_FIELD_TAG,"Height",state.height.getField(),null))
+                   add(NameFieldUIModel(FieldsTags.WEIGHT_FIELD_TAG,"Weight",state.height.getField(),null))
+                   add(NameFieldUIModel(FieldsTags.SHORT_ADDRESS_FIELD_TAG,"Short Address",state.shortAddress.getField(),null))
+               }
             }
         )
 
 
     fun getSexesList() = ProfileSex.getAllSexes().map { it.sexName() }
     private fun ProfileSex.sexName() = this.getSexName(context)
+
+    private fun String?.getField() = this ?: ""
+    private fun ProfileSex?.getField() = this?.sexName() ?: ""
+    private fun LocalDate?.getField() = this?.let { "TODO" } ?: ""
 
 }
--- feature/profile/generalinfo/edit/src/main/java/com/aya/digital/core/feature/profile/generalinfo/edit/viewmodel/ProfileGeneralInfoEditState.kt
+++ feature/profile/generalinfo/edit/src/main/java/com/aya/digital/core/feature/profile/generalinfo/edit/viewmodel/ProfileGeneralInfoEditState.kt
@@ -1,11 +1,18 @@
 package com.aya.digital.core.feature.profile.generalinfo.edit.viewmodel
 
+import com.aya.digital.core.model.ProfileSex
 import com.aya.digital.core.mvi.BaseState
 import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
 
 @Parcelize
-data class ProfileGeneralInfoEditState(val email: String = "",
-                                       val emailError: String? = null,
-                                       val password: String = "",
-                                       val passwordError: String? = null
-                       ) : BaseState
+data class ProfileGeneralInfoEditState(
+    val firstName: String = "",
+    val lastName: String = "",
+    val middleName: String = "",
+    val birthDate : LocalDate? = null,
+    val sex:ProfileSex? = null,
+    val height:String? = null,
+    val weight:String? = null,
+    val shortAddress:String? = null
+) : BaseState
