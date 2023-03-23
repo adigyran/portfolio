997123bb18dbd4480a052e2ed1a192413d7aeaf1
adigyran <adigyran@gmail.com>
2023-03-23 12:58:44 +0300

    homepage

--- core/network/main/src/main/java/com/aya/digital/core/network/main/Constants.kt
+++ core/network/main/src/main/java/com/aya/digital/core/network/main/Constants.kt
@@ -10,8 +10,8 @@ internal object Constants {
     const val RETROFIT_BUILDER_TOKEN = "retrofitBuilderToken"
 
 
-    const val BASE_URL_API = "https://api2.aya-doc.com/api/"
-    const val BASE_AUTH_URL_API = "https://aya-doc.com/api-mobile-auth/"
+    const val BASE_URL_API = "https://api-v3.aya-doc.com/api/"
+    const val BASE_AUTH_URL_API = "https://auth-v3.aya-doc.com/api-mobile-auth/"
     const val BASE_TOKEN_URL_API = "https://auth-test.aya-doc.com/auth/realms/aya-realm/protocol/"
     const val MOCK_URL = "https://63026b6e-ca23-46b4-85d4-d2382905efac.mock.pstmn.io"
     const val MOCK_API_KEY = "PMAK-62f26d6b174b441e28eefe75-cb097e91667b257d714730adc3af89d743"
