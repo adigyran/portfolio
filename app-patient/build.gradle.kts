bac7d86a64ea2f56042ace47fac0dad686714993
adigyran <adigyran@gmail.com>
2023-03-29 20:14:13 +0300

    version 8

--- app-patient/build.gradle.kts
+++ app-patient/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 
     defaultConfig {
         applicationId = "com.aya.digital.healthapp.patient"
-        versionCode = 7
-        versionName = "0.0.7" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 8
+        versionName = "0.0.8" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
