f774e96f1e84afe9575641eb8980b78b1347e508
Yulia <yulia.nabokova@aya-dev.com>
2023-06-29 12:27:23 +0300

    version 39

--- app-patient/build.gradle.kts
+++ app-patient/build.gradle.kts
@@ -17,8 +17,8 @@ android {
 
     defaultConfig {
         applicationId = "com.aya.digital.healthapp.patient"
-        versionCode = 37
-        versionName = "0.0.37" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 39
+        versionName = "0.0.39" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
