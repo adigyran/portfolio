4c12aff696cda206900d13b1d6004ff5598766b6
Yulia <yulia.nabokova@aya-dev.com>
2023-07-03 19:43:08 +0300

    viewgroup for filters

--- feature/tabviews/doctorsearch/build.gradle.kts
+++ feature/tabviews/doctorsearch/build.gradle.kts
@@ -11,4 +11,6 @@ dependencies {
     implementation(project(":core:domain:doctors"))
     implementation(project(":core:domain:base"))
     implementation(project(":core:ui:delegates:features:doctors:doctoritem"))
+    implementation(project(":feature:doctors:doctorsfiltersview"))
+
 }
