9ba858d236e495749806a8efdaa16bd74bd3a0f6
Yulia <yulia.nabokova@aya-dev.com>
2023-07-19 18:41:16 +0300

    doctors avatar renderer

--- core/di/src/main/java/com/aya/digital/core/di/modules/DomainDiModules.kt
+++ core/di/src/main/java/com/aya/digital/core/di/modules/DomainDiModules.kt
@@ -4,16 +4,17 @@ import com.aya.digital.core.domain.appointment.di.appointmentsDomainDiModule
 import com.aya.digital.core.domain.auth.di.authDomainDiModule
 import com.aya.digital.core.domain.dictionaries.di.dictionariesDomainDiModule
 import com.aya.digital.core.domain.doctors.di.doctorsDomainDiModule
-import com.aya.digital.core.domain.schedule.di.scheduleDomainDiModule
 import com.aya.digital.core.domain.profile.di.profileDomainDiModule
-import com.aya.digital.core.domain.profile.di.rootDomainDiModule
+import com.aya.digital.core.domain.schedule.di.scheduleDomainDiModule
+import com.aya.digital.core.domain.root.di.rootDomainDiModule
 
 fun domainDiModules() = listOf(
     authDomainDiModule(),
-    profileDomainDiModule(),
-    dictionariesDomainDiModule(),
     doctorsDomainDiModule(),
+    dictionariesDomainDiModule(),
     appointmentsDomainDiModule(),
+    profileDomainDiModule(),
     scheduleDomainDiModule(),
     rootDomainDiModule()
 )
+
