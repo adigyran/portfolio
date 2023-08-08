2cb465c6af6b3f6ce61eed998c2b95af3760f359
Yulia <yulia.nabokova@aya-dev.com>
2023-08-08 15:52:12 +0300

    video call, keep screen on experiment

--- feature/videocall/videocallscreen/src/main/java/com/aya/digital/core/feature/videocall/videocallscreen/ui/VideoCallScreenView.kt
+++ feature/videocall/videocallscreen/src/main/java/com/aya/digital/core/feature/videocall/videocallscreen/ui/VideoCallScreenView.kt
@@ -14,6 +14,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -346,6 +347,7 @@ class VideoCallScreenView :
 
     override fun prepareCreatedUi(savedInstanceState: Bundle?) {
         super.prepareCreatedUi(savedInstanceState)
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         localVideoView = contentVideoBinding.primaryVideoView
 
