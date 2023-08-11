cd9ba97325bef66fdf398a0b4db35e36d5abd418
yulia <adigyran@gmail.com>
2023-08-11 19:59:32 +0300

    version 55

--- app-patient/build.gradle.kts
+++ app-patient/build.gradle.kts
@@ -17,8 +17,8 @@ android {
 
     defaultConfig {
         applicationId = "com.aya.digital.healthapp.patient"
-        versionCode = 54
-        versionName = "0.0.54" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 55
+        versionName = "0.0.55" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
