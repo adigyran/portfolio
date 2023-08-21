bf1fd7882befd76c4371b9311349b763c8cc6ef8
yulia <adigyran@gmail.com>
2023-08-21 19:32:17 +0300

    patient fields and doctor fields

--- feature/profile/generalinfo/edit/src/main/java/com/aya/digital/core/feature/profile/generalinfo/edit/viewmodel/ProfileGeneralInfoEditState.kt
+++ feature/profile/generalinfo/edit/src/main/java/com/aya/digital/core/feature/profile/generalinfo/edit/viewmodel/ProfileGeneralInfoEditState.kt
@@ -1,18 +1,27 @@
 package com.aya.digital.core.feature.profile.generalinfo.edit.viewmodel
 
 import android.net.Uri
+import android.os.Parcelable
 import com.aya.digital.core.model.ProfileSex
 import com.aya.digital.core.mvi.BaseState
-import kotlinx.datetime.LocalDate
 import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
+import java.time.LocalDate
 
 @Parcelize
 data class ProfileGeneralInfoEditState(
     val firstName: String? = null,
     val lastName: String? = null,
     val middleName: String? = null,
-    val dateOfBirth: @RawValue LocalDate? = null,
+    val dateOfBirth: LocalDate? = null,
+    val patientFields: PatientFields? = null,
+    val doctorFields: DoctorFields? = null,
+    val avatarUri:Uri? = null,
+    val avatarUrl:String = ""
+) : BaseState
+
+
+@Parcelize
+data class PatientFields(
     val sex: ProfileSex? = null,
     val height: String? = null,
     val heightError: String? = null,
@@ -21,6 +30,18 @@ data class ProfileGeneralInfoEditState(
     val shortAddress: String? = null,
     val ssnOrTin: String? = null,
     val ssnOrTinError: String? = null,
-    val avatarUri:Uri? = null,
-    val avatarUrl:String = ""
-) : BaseState
+): Parcelable
+
+@Parcelize
+data class DoctorFields(
+    val npi: String? = null,
+    val npiError:String? = null,
+    val tin: String? = null,
+    val tinError:String? = null,
+    val licenseNumber: String? = null,
+    val licenseNumberError:String? = null,
+    val languages: String? = null,
+    val languagesError:String? = null,
+    val bio: String? = null,
+    val bioError: String? = null,
+): Parcelable
--- feature/profile/generalinfo/view/src/main/java/com/aya/digital/core/feature/profile/generalinfo/view/viewmodel/ProfileGeneralInfoViewState.kt
+++ feature/profile/generalinfo/view/src/main/java/com/aya/digital/core/feature/profile/generalinfo/view/viewmodel/ProfileGeneralInfoViewState.kt
@@ -1,10 +1,11 @@
 package com.aya.digital.core.feature.profile.generalinfo.view.viewmodel
 
+import android.os.Parcelable
 import com.aya.digital.core.model.ProfileSex
 import com.aya.digital.core.mvi.BaseState
-import kotlinx.datetime.LocalDate
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
+import java.time.LocalDate
 
 @Parcelize
 data class ProfileGeneralInfoViewState(
@@ -13,11 +14,28 @@ data class ProfileGeneralInfoViewState(
     val firstName: String? = null,
     val lastName: String? = null,
     val middleName: String? = null,
-    val dateOfBirth: @RawValue LocalDate? = null,
+    val dateOfBirth: LocalDate? = null,
+    val patientFields: PatientFields? = null,
+    val doctorFields: DoctorFields? = null
+
+) : BaseState, Parcelable
+
+@Parcelize
+data class PatientFields(
     val ssn: String? = null,
     val tin: String? = null,
     val sex: ProfileSex? = null,
     val height: String? = null,
     val weight: String? = null,
     val shortAddress: String? = null
-) : BaseState
+):Parcelable
+
+@Parcelize
+data class DoctorFields(
+    val npi: String? = null,
+    val tin: String? = null,
+    val licenseNumber: String? = null,
+    val language: String? = null,
+    val bio: String? = null,
+):Parcelable
+
