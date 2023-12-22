b026499048dd2a77c1a74b2360e62cc2491235c7
Yulia <yulia.nabokova@aya-dev.com>
2023-12-22 20:04:46 +0300

    get prescriptions

--- core/network/api/services/src/main/java/com/aya/digital/core/network/api/services/PrescriptionsService.kt
+++ core/network/api/services/src/main/java/com/aya/digital/core/network/api/services/PrescriptionsService.kt
@@ -8,24 +8,24 @@ import retrofit2.http.*
 
 interface PrescriptionsService {
 
-    @PATCH("settings/prescriptions/{id}")
+    @PATCH("api/settings/prescriptions/{id}")
     fun editPrescriptionByPrescriptionId(@Path("id") prescriptionId: Int, @Body prescriptionContentBody: PrescriptionContentBody): Single<Boolean>
 
-    @PATCH("settings/prescriptions/update-content-by-appId/{id}")
+    @PATCH("api/settings/prescriptions/update-content-by-appId/{id}")
     fun editPrescriptionByAppointmentId(@Path("id") appointmentId: Int, @Body prescriptionContentBody: PrescriptionContentBody): Single<Boolean>
 
-    @PUT("settings/prescriptions/subscribe")
+    @PUT("api/settings/prescriptions/subscribe")
     fun subscribeToPrescriptions(@Body prescriptionSubscribeBody: PrescriptionSubscribeBody): Completable
 
-    @GET("settings/prescriptions/get-content-by-appId/{id}")
+    @GET("api/settings/prescriptions/get-content-by-appId/{id}")
     fun getPrescriptionsByAppointmentId(@Path("id") appointmentId: Int): Single<Boolean>
 
-    @GET("settings/prescriptions/{id}")
+    @GET("api/settings/prescriptions/{id}")
     fun getPrescriptionById(@Path("id") prescriptionId: Int): Single<Boolean>
 
-    @GET("settings/prescriptions/patient-prescription-list/{id}")
+    @GET("api/settings/prescriptions/patient-prescription-list/{id}")
     fun getPatientPrescriptionsByPatientId(@Path("id") patientId: Int): Single<Boolean>
 
-    @GET("settings/prescriptions/practitioner-prescription-list/{id}")
+    @GET("api/settings/prescriptions/practitioner-prescription-list/{id}")
     fun getDoctorPrescriptionsByPractitionerId(@Path("id") practitionerId: Int): Single<Boolean>
 }
